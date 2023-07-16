4179498f928219625c7eb726d4dddcbf685f9707
Adrian Miarczyński <adrian331144@gmail.com>
2023-07-16 23:27:27 +0200

    changed pattern

--- src/components/contactsform/contactsform.jsx
+++ src/components/contactsform/contactsform.jsx
@@ -36,7 +36,7 @@ const ContactsForm = () => {
           className={css['form__input']}
           name="name"
           id="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          pattern="^[a-zA-Za]+(([' \-][a-zA-Za])?[a-zA-Za]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
         />
@@ -46,7 +46,7 @@ const ContactsForm = () => {
         <input
           type="tel"
           name="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          pattern="\+?\d{1,4}?[\-.\s]?\(?\d{1,3}?\)?[\-.\s]?\d{1,4}[\-.\s]?\d{1,4}[\-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
           id="number"
